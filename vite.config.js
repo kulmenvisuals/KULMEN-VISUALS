4a3a96bc07230db9c145b8c848364294e34e26d2
kulmenvisuals <ioelena1@gmail.com>
2026-01-03 03:46:59 +0100

    Fix base for custom domain

--- vite.config.js
+++ vite.config.js
@@ -4,5 +4,5 @@ import react from '@vitejs/plugin-react'
 export default defineConfig({
   plugins: [react()],
   // nombre EXACTO del repo entre barras:
-  base: '/KULMEN-VISUALS/',
+  base: '/',
 })
