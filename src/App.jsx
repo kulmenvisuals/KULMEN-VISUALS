73b0ca695055066c8dd8a9c72279fc1bb8e75441
kulmenvisuals <ioelena1@gmail.com>
2026-01-09 20:37:56 +0100

    scroll

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,14 @@
 import { Outlet, useLocation } from 'react-router-dom'
 import Header from './components/Header.jsx'
 import Footer from './components/Footer.jsx'
+import ScrollToTop from './components/ScrollToTop.jsx'
 
 function App() {
   const location = useLocation()
 
   return (
     <div className="min-h-screen bg-zinc-950 text-zinc-50 flex flex-col">
+      <ScrollToTop />
       <Header currentPath={location.pathname} />
 
       <main className="flex-1">
